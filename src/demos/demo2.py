2f9a820ae72ce064ee8eef58655ca8f725021c97
Tom Tarpey <tomtarpeydev@gmail.com>
2020-12-16 05:15:32 +0000

    added demo2 and some assets

--- src/demos/demo2.py
+++ src/demos/demo2.py
@@ -44,3 +44,38 @@ def are_words_sorted(words, alpha_order):
     bool
     """
     # Your code here
+    # init a dict to keep track of letters as keys and their altered indices as values
+    altered_order_dict = {letter: index for index, letter in enumerate(alpha_order)}
+
+    # this way we can look up each letter in our dictionary to figure out it's altered index
+    # once we have the altered indices we can check if they are in the right order
+
+    for i in range(1, len(words)):
+        w1 = words[i - 1]
+        w2 = words[i]
+        # iterate over each of the chars of the 2 words, checking that the current letters adhere to the altered order
+        for j in range(min(len(w1), len(w2))):
+            ch1 = w1[j]
+            ch2 = w2[j]
+
+            # check each of the letters
+            if ch1 != ch2:
+                if altered_order_dict[ch1] > altered_order_dict[ch2]:
+                    return False
+                else:
+                    break
+        
+        # if we end up falling out of the inner loop check if the length of w1 is greater than the length of w2
+        if len(w1) > len(w2):
+            # return False
+            return False
+
+    
+    # once we fall out of the outer loop if we did not already return then we must return true
+    return True
+
+
+
+print(are_words_sorted(["lambda", "school"], "hlabcdefgijkmnopqrstuvwxyz"))  # => True
+print(are_words_sorted(["were", "where", "yellow"], "habcdefgijklmnopqrstuvwxyz"))  # => False
+print(are_words_sorted(["lambda","lamb"], "abcdefghijklmnopqrstuvwxyz")) # => False
